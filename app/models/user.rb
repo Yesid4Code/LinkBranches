6138a6a3439cc76c807ad2ebabffae48a9085199
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-03-15 16:01:31 -0400

    most desirable entrances with sanitize_text method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  before_save :sanitize_text
+
+
+  def sanitize_text
+    self.username =username.downcase
+    self.name = name.titleize
+    self.email = email.downcase
+  end
 end
