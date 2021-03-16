8e0a53e9c006c637364a29a6c1776faae535d9cd
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-03-16 10:44:59 -0400

    validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   before_save :sanitize_text
+  
 
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true,
+            length: { minimun: 5, maximum: 10 }
+  validates :name, presence: true
+  validates :email, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: {maximum: 25},
+            format: {with: VALID_EMAIL_REGEX}
+  
   def sanitize_text
-    self.username =username.downcase
-    self.name = name.titleize
+    #self.username = username.downcase
+    #self.name = name.titleize
     self.email = email.downcase
   end
 end
