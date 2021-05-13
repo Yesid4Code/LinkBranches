2b34d763fc50080f36ba8eb6b6f0565b4ed20e00
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-05-12 21:19:19 -0400

    Big Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/profile'
+  #get 'users/edit'
+  #get 'users/profile'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
